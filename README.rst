418515c21376a22b58c3a48612087072982a1773
revesansparole <revesansparole@gmail.com>
2016-07-11 16:43:36 +0200

    ported back transfo from master

---
+++ README.rst
@@ -0,0 +1,20 @@
+========================
+openalea.container
+========================
+
+.. {# pkglts, doc
+
+
+.. image:: https://readthedocs.org/projects/openalea-container/badge/?version=latest
+    :alt: Documentation status
+    :target: https://openalea-container.readthedocs.io/en/latest/?badge=latest
+
+
+.. image:: https://travis-ci.org/openalea/container.svg?branch=master
+    :alt: Travis build status
+    :target: https://travis-ci.org/openalea/container
+
+.. #}
+
+Container is a set of data structures used in openalea such as : graph, grid, topomesh
+
--- src/openalea/container/grid.py
+++ src/openalea/container/grid.py
@@ -13,75 +13,117 @@
 #
 #       VPlants WebSite : https://gforge.inria.fr/projects/vplants/
 #
-
-__doc__="""
-This module provide a simple pure python implementation
+"""This module provide a simple pure python implementation
 for a grid interface
 """
 
-__license__= "Cecill-C"
-__revision__=" $Id$ "
 
-from interface.grid import IGrid,ICaseListGrid
-
-class Grid (IGrid,ICaseListGrid) :
-    """
-    interface definition of simple N dimensional grids
-    with finite number of case per dimension
+class Grid(object):
+    """Interface definition of simple N dimensional grids
+    with finite number of cell per dimension
     """
 
-    def __init__ (self, shape) :
+    def __init__(self, shape):
+        """Constructor of a finite grid.
+
+        args:
+         - shape (iter of int): number of cell in each dimension
         """
-        constructor of a finite grid
-        :param shape: number of case in each dimension
-        :type shape: iter of int
-        """
-        self._shape=[int(s) for s in shape]
-        offset=[1]
-        for i,incr in enumerate(self._shape[:-1]) :
-            offset.append(offset[i]*incr)
-        self._offset=offset
+        self._shape = [int(s) for s in shape]
+        offset = [1]
+        for i, incr in enumerate(self._shape[:-1]):
+            offset.append(offset[i] * incr)
+        self._offset = offset
 
     # ##########################################################
     #
     #               Grid concept
     #
     # ##########################################################
-    def dim (self) :
+    def dim(self):
+        """Dimension of the grid, i.e. number of axes.
+
+        return:
+         - (int)
+        """
         return len(self._shape)
-    dim.__doc__=IGrid.dim.__doc__
 
-    def shape (self) :
+    def shape(self):
+        """Shape of the grid, i.e. number of cell along each axis.
+
+        return:
+         - (iter of int)
+        """
         return iter(self._shape)
-    shape.__doc__=IGrid.shape.__doc__
 
     # ##########################################################
     #
-    #               Case list concept
+    #               Cell list concept
     #
     # ##########################################################
-    def __len__ (self) :
-        s=1
-        for incr in self._shape : s*=incr
+    def __len__(self):
+        """Total number of boxes in the grid.
+
+        return:
+         - (int)
+        """
+        if len(self._shape) == 0:
+            return 0
+
+        s = 1
+        for incr in self._shape:
+            s *= incr
         return s
-    __len__.__doc__=ICaseListGrid.__len__.__doc__
 
-    def __iter__ (self) :
+    def __iter__(self):
+        """Iterate on index of each cell in the grid.
+
+        return:
+         - (iter of int)
+        """
         return iter(xrange(self.__len__()))
-    __iter__.__doc__=ICaseListGrid.__iter__.__doc__
 
-    def index (self, coord ) :
-        return sum([coord[i]*offset for i,offset in enumerate(self._offset)])
-    index.__doc__=ICaseListGrid.index.__doc__
+    def index(self, coord):
+        """Compute the index of a cell from his position
+        inverse function of `coordinates`
 
-    def coordinates (self, ind) :
-        if not (0<=ind<len(self)) :
-            raise IndexError("index out of range index: %d max : %d" % (ind,len(self)))
-        reste=ind
-        coord=[]
-        for i in xrange(self.dim()-1,-1,-1) :
-            coord.append(reste/self._offset[i])
-            reste=reste%self._offset[i]
+        args:
+         - coord (tuple of int): position along each axis
+
+        return:
+         - (int)
+        """
+        ind = 0
+        for i, offset in enumerate(self._offset):
+            if not (0 <= coord[i] < self._shape[i]):
+                msg = "coord (%d) along axis %d not valid" % (coord[i], i)
+                raise IndexError(msg)
+
+            ind += coord[i] * offset
+
+        return ind
+
+    def coordinates(self, ind):
+        """Compute the position along each axis from the index of the cell.
+        inverse function of `index`
+
+        args:
+         - ind (int): index of the cell
+
+        return:
+         - (tuple of int)
+        """
+        imax = len(self)
+        if not (0 <= ind < imax):
+            msg = "index out of range index: %d max : %d" % (ind, imax)
+            raise IndexError(msg)
+
+        residue = ind
+        coord = []
+        for i in xrange(self.dim() - 1, -1, -1):
+            coord.append(residue / self._offset[i])
+            residue %= self._offset[i]
+
         coord.reverse()
-        return coord
-    coordinates.__doc__=ICaseListGrid.coordinates.__doc__
+
+        return tuple(coord)
